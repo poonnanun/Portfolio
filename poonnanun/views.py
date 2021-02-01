34a25446f10747fb5bd6020d2bb857d3e6f097f3
Poonnanun Poonnopathum <poonnanun1@gmail.com>
2021-02-01 17:54:10 +0700

    temporary disable button

--- poonnanun/views.py
+++ poonnanun/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from poonnanun.models import Contact
 
 def index(request):
     return render(request, 'index.html')
 
 def contact(request):
-    return render(request, 'index.html')
+
+    return render(request, 'contact.html')
